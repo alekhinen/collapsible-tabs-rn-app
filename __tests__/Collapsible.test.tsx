6f3ab6533e6d9d3fac228192171ca04df337745b
Nick Alekhine <nick.alekhine@gmail.com>
2022-02-22 11:55:14 -0600

    update test files

--- __tests__/Collapsible.test.tsx
+++ __tests__/Collapsible.test.tsx
@@ -5,8 +5,22 @@ import Collapsible from '../Collapsible';
 
 import renderer from 'react-test-renderer';
 
-it('should render a tab navigation', () => {
+it('should render tab navigation synchronously', () => {
   const inst = renderer.create(<Collapsible />);
 
   expect(inst.root.findByProps({testID: 'view-1'})).not.toBeNull();
+});
+
+xit('TEST NEVER COMPLETES - should render tab navigation asynchronously', async () => {
+  const inst = renderer.create(<Collapsible />);
+
+  await new Promise(resolve => setTimeout(resolve, 0));
+
+  expect(inst.root.findByProps({testID: 'view-1'})).not.toBeNull();
+});
+
+xit('TEST NEVER COMPLETES - should render tab navigation with testing library', () => {
+  const {getByTestId} = render(<Collapsible />);
+
+  expect(getByTestId('view-1')).not.toBeNull();
 });
